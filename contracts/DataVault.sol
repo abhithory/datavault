b7bd738e1c4f321dfacda73bdf5d2ea152f82163
theteamisbest <abhithory@gmail.com>
2023-04-25 19:56:39 +0530

    basic smart contract added

--- contracts/DataVault.sol
+++ contracts/DataVault.sol
@@ -5,6 +5,43 @@ pragma solidity ^0.8.9;
 // import "hardhat/console.sol";
 
 contract DataVault {
+    // structure of file data
+    struct FileStruct{
+        string name;
+    }
+    
+    // structure of Credential data
+    struct Credential{
+        string name;
+    }
 
+    // event FileAddedByUser(string name,address indexed user);
+    // event CredentialAddedByUser(string name,address indexed user);
+
+    // mapping of all files of user
+    mapping(address=>FileStruct[]) private _allFilesOfUser; // addressOfUser => files[]
+
+    //  mapping of all credentials of user
+    mapping(address=>Credential[]) private _allCredentialsOfUser; //addressOfUser => credentials[]
+
+
+    function addFileOfUser(FileStruct memory _fileData) external {
+        _allFilesOfUser[msg.sender][userTotalFilesCount()] = _fileData; 
+    }
+
+    function addCredentialOfUser(Credential memory _credentialData) external {
+        _allCredentialsOfUser[msg.sender][userTotalCredentialCount()] = _credentialData; 
+    }
+
+
+    // get total files count that user added
+    function userTotalFilesCount() internal view returns (uint256){
+        return _allFilesOfUser[msg.sender].length;
+    }
+
+    // get total credentials count that user added
+    function userTotalCredentialCount() internal view returns (uint256){
+        return _allCredentialsOfUser[msg.sender].length;
+    }
 
 }
