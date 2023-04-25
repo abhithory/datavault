68835102f99974e11dc3b0e74832a96e0a23f3d6
theteamisbest <abhithory@gmail.com>
2023-04-26 01:04:26 +0530

    basic file upload system created

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,38 @@
 import React from 'react'
 
 export default function Home() {
+
+    function handleFormFile(e: React.ChangeEvent<HTMLFormElement>){
+        e.preventDefault();
+        console.log('===============handleFormFile=====================');
+        // console.log(e.target.userfile.files);
+        // const firstName = event.target.firstName.value;
+
+        // here upload file on ipfs
+
+        console.log('====================================');
+    }
+
+    function handleFileUpload(e: React.ChangeEvent<HTMLInputElement>){
+        console.log('============handleFileUpload========================');
+        console.log(e.target.files);
+
+        // show basic details of file to user
+        // name, size, type
+        console.log('====================================');
+    }
   return (
-    <div>Home</div>
+    <div>
+        <button>Connect wallet</button>
+        <p>address: {}</p>
+
+        <h1>upload file</h1>
+        <form onSubmit={handleFormFile}>
+            <input type="file" name='userfile' id='userfile' onChange={handleFileUpload} />
+            <br />
+            <button type='submit'>Upload file</button>
+        </form>
+
+    </div>
   )
 }
