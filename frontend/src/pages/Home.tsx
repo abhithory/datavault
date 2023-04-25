3f1572ad283f2329dd76ceddd613c490890fa0a4
theteamisbest <abhithory@gmail.com>
2023-04-26 03:07:27 +0530

    file upload on ipfs using spheron

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,27 +1,13 @@
 import React from 'react'
+import { upload } from "@spheron/browser-upload";
 
 
-
-// import { SpheronClient, ProtocolEnum, UploadStatusEnum } from "@spheron/storage";
-// const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlLZXkiOiJiYTdmYzNkOGQ4N2M1ZmEyNWNlMDI4YmYyNTQyNzBmMjJlZmEzZDFjZGExNDFmZDk2NTRiOWU0NTJkZmNmNzlhZTUwNWZjNTA1NmNmMDVmODYxYzRmNzFkYTcwZWU4N2ZjNDJjZDA5NTAzN2E3ZmExNGNjODFiNmU4YmJhMjQ5OSIsImlhdCI6MTY4MjQ1MjQ2NSwiaXNzIjoid3d3LnNwaGVyb24ubmV0d29yayJ9.kU60B94qXvWQ8JzHhuZ2H4x7QtSGpKCpIpybhf2fwSo"
-// const client = new SpheronClient({ token });
 
 export default function Home() {
 
-    // interface Upload {
-    //     id: string;
-    //     protocolLink: string;
-    //     buildDirectory: string[];
-    //     status: UploadStatusEnum;
-    //     memoryUsed: number;
-    //     bucketId: string;
-    //     protocol: string;
-    //   }
-
-
     // async function uploadFile(filePath:string, name:string) {
     //     console.log("uploading file");
-        
+
     //     // const filePath = "";
     //     // const name = ""
     //     let currentlyUploaded:number = 0;
@@ -51,29 +37,43 @@ export default function Home() {
     //     // console.log(upload);
     // }
 
-    function handleFormFile(e: React.ChangeEvent<HTMLFormElement>) {
+    async function handleFormFile(e: React.ChangeEvent<HTMLFormElement>) {
         e.preventDefault();
         console.log('===============handleFormFile=====================');
-        // console.log(e.target.userfile.files);
-        // const firstName = event.target.firstName.value;
+        const _file: File = e.target.userfile.files[0];
+        console.log(_file);
 
         // here upload file on ipfs
+        const response = await fetch(`http://localhost:8000/api/v1/getuploadtoken`); // from step 1
+        const resJson = await response.json();
+        const token = resJson.uploadToken;
+        // const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkZXBsb3ltZW50SWQiOiI2NDQ4NDVlMDIwMjc3YTAwMTJlOTNiOTciLCJzaW5nbGVEZXBsb3ltZW50Ijp0cnVlLCJwYXlsb2FkU2l6ZSI6NTI0Mjg4MCwicGFyYWxsZWxVcGxvYWRDb3VudCI6NSwiaWF0IjoxNjgyNDU4MDgwLCJleHAiOjE2ODI0NTg2ODAsImlzcyI6Ind3dy5zcGhlcm9uLm5ldHdvcmsifQ.F2n0q4iyysHklirBjs2Vrz6bKNui5i8aZy--HBvb8TQ"
+        console.log(token);
 
+        let currentlyUploaded = 0;
+        const uploadResult = await upload([_file], {
+            token, onChunkUploaded: (uploadedSize, totalSize) => {
+                currentlyUploaded += uploadedSize;
+                console.log(`Uploaded ${currentlyUploaded} of ${totalSize} Bytes.`);
+            },
+        });
+
+        console.log("uploadResult", uploadResult.protocolLink);
         console.log('====================================');
     }
 
     function handleFileUpload(e: React.ChangeEvent<HTMLInputElement>) {
         console.log('============handleFileUpload========================');
-        
+
         let file: any = e.target.files && e.target.files[0]
         console.log(file);
 
-        let reader = new FileReader();
+        // let reader = new FileReader();
 
-        reader.onloadend = () => {
-          console.log("url",reader.result);          
-        }
-        reader.readAsDataURL(file);
+        // reader.onloadend = () => {
+        //   console.log("url",reader.result);          
+        // }
+        // reader.readAsDataURL(file);
 
 
         // show basic details of file to user
