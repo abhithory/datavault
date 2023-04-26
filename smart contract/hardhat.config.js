756c4bd29498fb5b77e7c19bc54a9c2cb2a399e9
theteamisbest <abhithory@gmail.com>
2023-04-26 11:55:19 +0530

    shardeum sphinx network added in hardhat

--- smart contract/hardhat.config.js
+++ smart contract/hardhat.config.js
@@ -2,5 +2,12 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.9",
+  networks:{
+    shardeumSphinx:{
+        url: "https://sphinx.shardeum.org/",
+        "chainId":8082,
+        accounts: ["695852bea31a5ea016ed48145d87b51bbefbd639609a3a77188253abf5938ff9"]
+    }
+  }
 };
