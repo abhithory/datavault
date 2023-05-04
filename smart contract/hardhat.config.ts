8730215cfa788f793c9568bbd0f5d317f89b43e3
theteamisbest <abhithory@gmail.com>
2023-05-04 12:46:00 +0530

    local network added

--- smart contract/hardhat.config.ts
+++ smart contract/hardhat.config.ts
@@ -10,8 +10,14 @@ const config: HardhatUserConfig = {
         url: "https://liberty20.shardeum.org",
         "chainId":8081,
         accounts: ["695852bea31a5ea016ed48145d87b51bbefbd639609a3a77188253abf5938ff9"]
-    }
+    },
+    local:{
+      url: "HTTP://127.0.0.1:7545",
+      "chainId":1337,
+      accounts: ["ddda13a3964b9f9ebe4dff462e137519ef1f4b2914032a23e389db7e452e650e"]
+  }
   }
 };
 
+// npx hardhat run .\scripts\deploy.ts --network local
 export default config;
