cad307c33bbc5505cc9c06f7351304313b692b00
theteamisbest <abhithory@gmail.com>
2023-05-06 15:44:09 +0530

    new network added + env

--- smart contract/hardhat.config.ts
+++ smart contract/hardhat.config.ts
@@ -1,16 +1,24 @@
 
 import { HardhatUserConfig } from "hardhat/config";
 require("@nomicfoundation/hardhat-toolbox");
+import { config as dotEnvConfig } from "dotenv";
+dotEnvConfig();
+
 
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
   networks:{
-    shardeumSphinx:{
+    shardeumLiberty:{
         url: "https://liberty20.shardeum.org",
         "chainId":8081,
-        accounts: ["695852bea31a5ea016ed48145d87b51bbefbd639609a3a77188253abf5938ff9"]
+        accounts: [process.env.PRIVATE_KEY as string]
     },
+    shardeumSphinx:{
+      url: "https://sphinx.shardeum.org/",
+      "chainId":8082,
+      accounts: [process.env.PRIVATE_KEY as string]
+  },
     local:{
       url: "HTTP://127.0.0.1:7545",
       "chainId":1337,
